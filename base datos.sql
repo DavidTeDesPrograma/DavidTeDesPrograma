1f4a95b6b393c42a20fd16204f4a68c7c0efb3c1
DavidTeDesPrograma <152659103+DavidTeDesPrograma@users.noreply.github.com>
2024-03-04 19:20:18 -0400

    Update base datos.sql

--- base datos.sql
+++ base datos.sql
@@ -28,9 +28,41 @@ Cantidad VARCHAR(50),
 PRIMARY KEY (ID_Producto)
 );
 
+CREATE TABLE Producto_Factura(
 
-/*CREATE TABLE Usuario(
-ID_Usuario int,
- Usuario VARCHAR(59)
- Contraseña
-);*/
+ID_Producto_Factura int,
+
+ Usuario VARCHAR(50),
+
+ Contraseña VARCHAR(50),
+ 
+ PRIMARY KEY (ID_Producto_Factura)
+);
+
+CREATE TABLE Factura(
+
+ID_Factura int,
+
+ Comprador VARCHAR(50),
+
+ Contraseña varchar(50)
+ );
+
+CREATE TABLE Clientes(
+
+ ID_Clientes INT,
+ 
+ Codigo VARCHAR(59),
+
+ Nombre VARCHAR(50),
+ 
+ Direccion VARCHAR(50),
+ 
+ Telefono VARCHAR(50),
+ 
+ Fax VARCHAR(50),
+ 
+ Correo VARCHAR(20),
+
+Estado ENUM('FIAO', 'DEBE','PAGO COMPLETO') 
+);
